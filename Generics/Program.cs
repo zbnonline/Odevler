d3af5a0a7f01b5a1b24aac036434d105bd0b8cb5
anıl <nbr-nb@hotmail.com>
2023-06-08 16:08:09 +0300

    MyDictionary araştırıldı ve uygulandı

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Generics
 {
@@ -6,7 +7,40 @@ namespace Generics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<string> sehirler = new List<string>();
+            sehirler.Add("Eskişehir");
+            Console.WriteLine(sehirler.Count);
+
+            MyList<string> sehirler2 = new MyList<string>();
+            sehirler2.Add("Eskişehir");
+            Console.WriteLine(sehirler2.Count);
+        }
+    }
+
+    class MyList<T> // Generic class 
+    {
+        T[] _array;
+        T[] _tempArray;  // 2 tane boş array üretiyoruz
+        public MyList()
+        {
+            _array = new T[0]; // Arrayi sıfır elemanlı olarak başlatıyoruz
+        }
+        public void Add(T item)
+        {
+            _tempArray = _array; // Eski verileri kaybetmemek için verileri tempArray'e aktarıyoruz
+            _array = new T[_array.Length + 1]; // Array'imizin eleman sayısını gönderdiğimiz item'ı eklemek için bir arttırıyoruz. 
+
+            for (int i = 0; i < _tempArray.Length; i++)
+            {
+                _array[i] = _tempArray[i]; // _tempArray'e aktardığımız verileri tekrar _array içine aktarıyoruz
+            }
+
+            _array[_array.Length - 1] = item; // arrayın eleman sayısını bulup son boş kalan yere gönderdiğimiz item'ı yazdırıyoruz
+        }
+
+        public int Count
+        {
+            get { return _array.Length; }
         }
     }
 }
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+
+            myDictionary.Add(1, "Ömer");
+            myDictionary.Add(2, "Zabunoğlu");
+            myDictionary.See();
+
+        }
+    }
+    class MyDictionary<TKey, TValue>
+    {
+        TKey[] keys;
+        TValue[] values;
+        public MyDictionary()
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            TKey[] _tempKeys = keys;
+            TValue[] _tempvalue = values;
+            keys = new TKey[keys.Length + 1];
+            values = new TValue[values.Length + 1];
+
+            for (int i = 0; i < _tempKeys.Length; i++)
+            {
+                keys[i] = _tempKeys[i];
+                values[i] = _tempvalue[i];
+            }
+            keys[keys.Length - 1] = key;
+            values[keys.Length - 1] = value;
+        }
+        public void See()
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Console.WriteLine(keys[i] + " : " + values[i]);
+            }
+        }
+        public TKey[] Keys
+        {
+            get { return keys; }
+        }
+
+        public TValue[] Values
+        {
+            get { return values; }
+        }
+
+
+    }
+}
