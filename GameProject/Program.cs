a03cfbb88ead246632d6feb2e475b067233ac8c7
anıl <nbr-nb@hotmail.com>
2023-06-14 17:12:26 +0300

    GameProject Add

---
+++ GameProject/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GameProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //GamerManager gamerManager = new GamerManager(new UserValidationManager());
+            //gamerManager.Add(new Gamer() { Id = 1, FirstName = "ÖMER", LastName = "ZABUNOĞLU", BirthYear = 2000, IdentityNumber = 616161 });
+
+            float x = 22 / 5;
+            Console.WriteLine(x);
+        }
+    }
+}
---
+++ GameProject/UserValidationManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProject
+{
+    class UserValidationManager : IUserValidationService
+    {
+        public bool Validate(Gamer gamer)
+        {
+            if (gamer.FirstName == "ÖMER" && gamer.LastName == "ZABUNOĞLU" && gamer.BirthYear == 2000 && gamer.IdentityNumber == 616161 )
+            {
+                return true;
+            } 
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
